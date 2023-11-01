31707e6c932c1ea74b0e4249762ea2a31093b6d0
nerydyego <dyegonery@gmail.com>
2023-11-01 13:44:35 -0300

    lista de tarefas

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('helloword/', views.helloword),
     path('', views.tasklist, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
     path('yourname/<str:name>', views.yourName, name='your-name')
 ]
