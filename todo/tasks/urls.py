713d37965a69fda2e52d373628b9cf36e702eec7
nerydyego <dyegonery@gmail.com>
2023-11-12 10:43:43 -0300

    Edit todo

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloword/', views.helloword),
     path('', views.tasklist, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
-    path('yourname/<str:name>', views.yourName, name='your-name')
+    path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
 ]
